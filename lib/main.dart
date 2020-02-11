a80fa0ec59a8c60cefab6dce99df44859443638e
Vlasov Viktor <vvvlasov@activebc.ru>
2020-02-12 01:41:44 +0300

    add image carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,73 @@
+import 'dart:wasm';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/src/material/banner_theme.dart';
+import 'package:flutter/src/painting/text_style.dart';
 
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final List<String> imgListTwo = ['1', '2', '3', '4'];
 
 void main() => runApp(MyApp());
+
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
+
+  return result;
+}
+
+final List child = map<Widget>(
+  imgListTwo,
+      (index, i) {
+      return Container(
+        margin: EdgeInsets.all(5.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(children: <Widget>[
+            Image.asset('images/house/' + (index+1).toString() + '.jpg', fit: BoxFit.cover, width: 1000.0),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  'No. $index image',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      );
+
+  },
+).toList();
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -10,6 +75,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => RandomWords(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/second': (context) => HousePage(),
+      },
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +94,6 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: RandomWords(),
 
 
     );
@@ -35,21 +106,29 @@ class HousePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
           title: ListTile(
-            leading: Image.asset('images/logo.png'),
-            title: Text('База отдыха - Сабурово'),
+            title: Text('Домик на 4 человека'),
           )
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Text('Click button to back to Main Page'),
+            CarouselSlider(
+              items: child,
+              autoPlay: false,
+              enlargeCenterPage: true,
+              viewportFraction: 0.9,
+              aspectRatio: 2.0,
+            ),
+            Padding(
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Text('Сутки считаются с 12.00 до 14.00 следующего дня, соответственно, заезд в сектор не позднее 12.00, выезд из сектора не позднее 14.00. Въезд на территорию базы отдыха с 06.00 до 20.00. Если рыболов приехал позже 12.00, то сутки будут считаться уже идущими и закончатся в 14.00 следующего дня.  Минимальное нахождение в секторе двое суток. Стоимость посещения: 500руб./ сутки с человека. Сопровождающие женщины, дети до 12 лет могут находиться на водоеме  бесплатно.', style: Theme.of(context).textTheme.caption)),
             RaisedButton(
               textColor: Colors.white,
-              color: Colors.redAccent,
-              child: Text('Back to Main Page'),
+              color: Colors.lightBlueAccent,
+              child: Text('Забронировать'),
               onPressed: () {
-                // TODO
+                Navigator.pop(context);
               },
             )
           ],
@@ -58,6 +137,15 @@ class HousePage extends StatelessWidget {
     );
   }
 }
+
+//RaisedButton(
+//textColor: Colors.white,
+//color: Colors.lightBlueAccent,
+//child: Text('Вернуться на главную'),
+//onPressed: () {
+//Navigator.pop(context);
+//},
+//)
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
@@ -77,7 +165,8 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Future navigateToSubPage(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => HousePage()));
+//    Navigator.push(context, MaterialPageRoute(builder: (context) => HousePage()));
+    Navigator.pushNamed(context, '/second');
   }
 
   Widget _build_cards() {
